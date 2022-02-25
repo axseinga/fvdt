275f1cf6463763c738abff406894ce73457570d9
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-25 13:13:32 +0000

    add more validation to backend

--- src/server/model/userModel.js
+++ src/server/model/userModel.js
@@ -1,30 +1,60 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../database");
 
+const genderList = ["Gender 1", "Gender 2", "Gender 3"];
+
 class User extends Model {}
 
 User.init(
     {
         name: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: 2,
+            },
         },
         surname: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: 2,
+            },
         },
         email: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
         },
         phone: {
             type: DataTypes.STRING,
+            allowNull: true, // testing purposes, bug
+            validate: {
+                is: /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/,
+            },
         },
         gender: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [genderList],
+            },
         },
         dob: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isDate: true,
+            },
         },
         comment: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: 10,
+            },
         },
     },
     {
