6aca270f14244ad9edf940c33f8ff4629d43de6e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-03-13 20:50:39 +0000

    add http status codes and error handling to controllers

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,23 +1,63 @@
 const User = require("../model/userModel");
 
 exports.getAllUsers = async (req, res) => {
-    const users = await User.findAll();
-    res.send(users);
+    try {
+        const users = await User.findAll();
+        res.status(200).json({
+            success: true,
+            data: { users },
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Server error",
+        });
+    }
 };
 
 exports.createUser = async (req, res) => {
-    await User.create(req.body);
-    res.send("user created");
+    try {
+        await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            msg: "User created",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Server error",
+        });
+    }
 };
 
 exports.getUser = async (req, res) => {
-    const reqId = req.params.id;
-    const user = await User.findOne({ where: { id: reqId } });
-    res.send(user);
+    try {
+        const reqId = req.params.id;
+        const user = await User.findOne({ where: { id: reqId } });
+        res.status(200).json({
+            success: true,
+            data: { user },
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Server error",
+        });
+    }
 };
 
 exports.deleteUser = async (req, res) => {
-    const reqId = req.params.id;
-    await User.destroy({ where: { id: reqId } });
-    res.send("user removed");
+    try {
+        const reqId = req.params.id;
+        await User.destroy({ where: { id: reqId } });
+        res.status(204).json({
+            success: true,
+            data: {},
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Server error",
+        });
+    }
 };
